c5d212d71eb08d9f6614c4c305ea0c8a3014f03d
Gabriel Dena <gabrieldenac@hotmail.com>
2024-06-28 11:20:12 -0300

    update lsp configs

--- lua/dena/lazy/lsp.lua
+++ lua/dena/lazy/lsp.lua
@@ -1,4 +1,3 @@
-
 return {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',
@@ -19,19 +18,8 @@ return {
             lsp_zero.default_keymaps({buffer = bufnr})
         end)
 
-        require('lspconfig').lua_ls.setup {
-            settings = {
-                Lua = {
-                    diagnostics = {
-                        globals = {
-                            'vim',
-                            'require'
-                        }
-                    }
-                }
-            }
-        }
         -- here you can setup the language servers
+
 
         -- to learn how to use mason.nvim
         -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
@@ -42,7 +30,32 @@ return {
                 function(server_name)
                     require('lspconfig')[server_name].setup({})
                 end,
+                ['lua_ls'] = function ()
+                    require('lspconfig').lua_ls.setup {
+                        settings = {
+                            Lua = {
+                                diagnostics = {
+                                    globals = {
+                                        'vim',
+                                        'require'
+                                    }
+                                }
+                            }
+                        },
+                    }
+                end
             },
+        })
+
+        local cmp = require('cmp')
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        cmp.setup({
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-Space>'] = cmp.mapping.complete(),
+            })
         })
     end
 }
