711b8cfe7945fe317123b7fc5287dd4c696843ee
Gabriel Dena <gabrieldenac@hotmail.com>
2024-08-22 09:53:59 -0300

    Update lsp.lua

--- lua/dena/lazy/lsp.lua
+++ lua/dena/lazy/lsp.lua
@@ -1,61 +1,113 @@
 return {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
+    "neovim/nvim-lspconfig",
     dependencies = {
-        'williamboman/mason.nvim',
-        'williamboman/mason-lspconfig.nvim',
-        'neovim/nvim-lspconfig',
-        'hrsh7th/nvim-cmp',
-        'hrsh7th/cmp-nvim-lsp',
-        'L3MON4D3/LuaSnip',
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "j-hui/fidget.nvim",
     },
-    config = function ()
-        local lsp_zero = require('lsp-zero')
 
-        lsp_zero.on_attach(function(client, bufnr)
-            -- see :help lsp-zero-keybindings
-            -- to learn the available actions
-            lsp_zero.default_keymaps({buffer = bufnr})
-        end)
+    config = function()
+        local cmp = require('cmp')
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities())
 
-        -- here you can setup the language servers
-
-
-        -- to learn how to use mason.nvim
-        -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
-        require('mason').setup({})
-        require('mason-lspconfig').setup({
-            ensure_installed = { 'angularls', 'dockerls', 'docker_compose_language_service', 'eslint', 'elixirls', 'tsserver', 'lua_ls', 'terraformls', 'vimls' },
+        require("fidget").setup({})
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = { 
+                    'angularls', 
+                    'dockerls', 
+                    'docker_compose_language_service', 
+                    'eslint', 
+                    'elixirls', 
+                    'tsserver', 
+                    'lua_ls', 
+                    'terraformls', 
+                    'vimls' 
+            },
             handlers = {
-                function(server_name)
-                    require('lspconfig')[server_name].setup({})
+                function(server_name) -- default handler (optional)
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
                 end,
-                ['lua_ls'] = function ()
-                    require('lspconfig').lua_ls.setup {
+
+                zls = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.zls.setup({
+                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+                        settings = {
+                            zls = {
+                                enable_inlay_hints = true,
+                                enable_snippets = true,
+                                warn_style = true,
+                            },
+                        },
+                    })
+                    vim.g.zig_fmt_parse_errors = 0
+                    vim.g.zig_fmt_autosave = 0
+
+                end,
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup {
+                        capabilities = capabilities,
                         settings = {
                             Lua = {
+                                runtime = { version = "Lua 5.1" },
                                 diagnostics = {
-                                    globals = {
-                                        'vim',
-                                        'require'
-                                    }
+                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
                                 }
                             }
-                        },
+                        }
                     }
-                end
-            },
+                end,
+            }
         })
 
-        local cmp = require('cmp')
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
         cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                end,
+            },
             mapping = cmp.mapping.preset.insert({
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
                 ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ['<C-Space>'] = cmp.mapping.complete(),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' }, -- For luasnip users.
+            }, {
+                { name = 'buffer' },
             })
+        })
+
+        vim.diagnostic.config({
+            -- update_in_insert = true,
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
         })
     end
 }
