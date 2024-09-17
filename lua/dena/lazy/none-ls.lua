f212ffd7854eb28bba8da07c5b3286cb695e13d0
Gabriel Dena <gabrieldenac@hotmail.com>
2024-09-17 16:40:33 -0300

    Create none-ls.lua

---
+++ lua/dena/lazy/none-ls.lua
@@ -0,0 +1,54 @@
+return {
+    'nvimtools/none-ls.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local null_ls = require('null-ls')
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.prettier.with({
+                    filetypes = {
+                        "javascript",
+                        "typescript",
+                        "css",
+                        "scss",
+                        "html",
+                        "json",
+                        "yaml",
+                        "markdown",
+                        "graphql",
+                        "md",
+                        "txt",
+                    },
+                    only_local = "node_modules/.bin",
+                }),
+                null_ls.builtins.formatting.stylua.with({
+                    filetypes = {
+                        "lua",
+                    },
+                    args = { "--indent-width", "2", "--indent-type", "Spaces", "-" },
+                }),
+                null_ls.builtins.diagnostics.stylelint.with({
+                    filetypes = {
+                        "css",
+                        "scss",
+                    },
+                }),
+            },
+
+            on_attach = function(client, bufnr)
+                if client.supports_method('textDocument/formatting') then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd('BufWritePre', {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ bufnr = bufnr })
+                        end,
+                    })
+                end
+            end,
+        })
+    end
+}
