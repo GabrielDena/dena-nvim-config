4cb5f08367219d9523a63726fe349d5da5cb392f
Gabriel Dena <gabrieldenac@hotmail.com>
2025-01-23 13:26:13 -0300

    harpoon: Change configs

--- lua/dena/lazy/harpoon.lua
+++ lua/dena/lazy/harpoon.lua
@@ -1,17 +1,18 @@
 return {
-    'theprimeagen/harpoon',
-    branch = 'harpoon2',
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
+        require("harpoon"):setup()
+    end,
+    keys = function()
         local harpoon = require("harpoon")
 
-        -- REQUIRED
-        harpoon:setup()
-        -- REQUIRED
-        -- basic telescope configuration
         local conf = require("telescope.config").values
+
         local function toggle_telescope(harpoon_files)
             local file_paths = {}
+
             for _, item in ipairs(harpoon_files.items) do
                 table.insert(file_paths, item.value)
             end
@@ -28,40 +29,108 @@ return {
                 })
             end
 
-            require("telescope.pickers").new({}, {
-                prompt_title = "Harpoon",
-                finder = require("telescope.finders").new_table({
-                    results = file_paths,
-                }),
-                previewer = conf.file_previewer({}),
-                sorter = conf.generic_sorter({}),
-                attach_mappings = function(prompt_buffer_number, map)
-                    -- The keymap you need
-                    map("i", "<c-d>", function()
-                        local state = require("telescope.actions.state")
-                        local selected_entry = state.get_selected_entry()
-                        local current_picker = state.get_current_picker(prompt_buffer_number)
+            require("telescope.pickers")
+                .new({}, {
+                    prompt_title = "Harpoon",
+                    finder = require("telescope.finders").new_table({
+                        results = file_paths,
+                    }),
+                    previewer = conf.file_previewer({}),
+                    sorter = conf.generic_sorter({}),
+                    layout_strategy = "center",
+                    layout_config = {
+                        preview_cutoff = 1, -- Preview should always show (unless previewer = false)
 
-                        -- This is the line you need to remove the entry
-                        harpoon:list():remove(selected_entry)
-                        current_picker:refresh(make_finder())
-                    end)
+                        width = function(_, max_columns, _)
+                            return math.min(max_columns, 80)
+                        end,
 
-                    return true
-                end,
-            }):find()
+                        height = function(_, _, max_lines)
+                            return math.min(max_lines, 15)
+                        end,
+                    },
+                    borderchars = {
+                        prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
+                        results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
+                        preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+                    },
+                    attach_mappings = function(prompt_buffer_number, map)
+                        map("i", "<C-d>", function()
+                            local state = require("telescope.actions.state")
+                            local selected_entry = state.get_selected_entry()
+                            local current_picker = state.get_current_picker(prompt_buffer_number)
+
+                            harpoon:list():remove(selected_entry)
+                            current_picker:refresh(make_finder())
+                        end)
+
+                        return true
+                    end,
+                })
+                :find()
         end
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-        vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
-            { desc = "Open harpoon window" })
+        return {
+            {
+                "<C-S-E>",
+                function()
+                    toggle_telescope(harpoon:list())
+                end,
+                desc = "Harpoon (Telescope)",
+            },
+            {
+                "<C-e>",
+                function()
+                    harpoon.ui:toggle_quick_menu(harpoon:list())
+                end,
+                desc = "Harpoon (Default)",
+            },
 
-        vim.keymap.set("n", "<C-t>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-s>", function() harpoon:list():select(3) end)
+            {
+                "<leader>a",
+                function()
+                    harpoon:list():add()
+                end,
+                desc = "Harpoon: Add",
+            },
 
-        -- Toggle previous & next buffers stored within Harpoon list
-        vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-        vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
-    end
+            {
+                "<C-t>",
+                function()
+                    harpoon:list():select(1)
+                end,
+            },
+
+            {
+                "<C-n>",
+                function()
+                    harpoon:list():select(2)
+                end,
+            },
+
+            {
+                "<C-s>",
+                function()
+                    harpoon:list():select(3)
+                end,
+            },
+
+            -- Toggle previous & next buffers stored within Harpoon list
+            {
+                "<C-S-P>",
+                function()
+                    harpoon:list():prev()
+                end,
+                desc = "Harpoon: Previous",
+            },
+            {
+                "<C-S-N>",
+                function()
+                    harpoon:list():next()
+                end,
+                desc = "Harpoon: Next",
+            },
+        }
+    end,
 }
+
